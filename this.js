f1f5f058f39c775b71ee3130cd60130cef49e0d6
nakib-abid <nakib.abid@gmail.com>
2024-09-29 13:46:48 +0600

    this

---
+++ this.js
@@ -0,0 +1,20 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    sleep(){
+        console.log(`Sleeping now ${this.name}`)
+    }
+    activity(){
+        this.sleep();
+    }
+}
+
+const kodom = new Person('Kodom Ali', 33);
+console.log(kodom);
+kodom.sleep();
+const badam = new Person('Kacha Badam', 23);
+console.log(badam);
+badam.sleep();
+
